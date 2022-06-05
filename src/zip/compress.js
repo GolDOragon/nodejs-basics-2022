3aa0dccd9a5181ade88bd2a010d3ad5261ddac40
Alexey Palanevich <alexey.palanevich@satelliteinnovations.io>
2022-06-05 22:44:48 +0300

    feat: zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,24 @@
+import { createReadStream, createWriteStream } from "fs";
+import path from "path";
+import { pipeline } from "stream/promises";
+import { fileURLToPath } from "url";
+import { createGzip } from "zlib";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+/**
+ * implement function that compresses file `fileToCompress.txt` to `archive.gz`
+ * using `zlib` and Streams API
+ */
 export const compress = async () => {
-    // Write your code here 
+  const filePath = path.resolve(__dirname, "files", "fileToCompress.txt");
+  const archivePath = path.resolve(__dirname, "files", "archive.gz");
+
+  const inputStream = createReadStream(filePath);
+  const zipStream = createGzip();
+  const outputStream = createWriteStream(archivePath);
+
+  pipeline(inputStream, zipStream, outputStream).catch((err) =>
+    console.log(err)
+  );
 };
