c3bae0203151da177585b2899454b44308eca099
Alexey Palanevich <alexey.palanevich@satelliteinnovations.io>
2022-06-05 09:49:42 +0300

    feat: streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,21 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+/**
+ * implement function that reads file `fileToRead.txt` content using Readable Stream and prints it's content into `process.stdout`
+ */
 export const read = async () => {
-    // Write your code here 
+  const input = fs.createReadStream(
+    path.resolve(__dirname, "files", "fileToRead.txt")
+  );
+
+  input.on("readable", () => {
+    const data = input.read();
+    if (data) {
+      process.stdout.write(data);
+    }
+  });
 };
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,16 @@
+import { createWriteStream } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+/**
+ *  implement function that writes process.stdin data into file `fileToWrite.txt` content using Writable Stream
+ */
 export const write = async () => {
-    // Write your code here 
+  const filePath = path.resolve(__dirname, "files", "fileToWrite.txt");
+
+  const output = createWriteStream(filePath, { flags: "a+" });
+
+  process.stdin.pipe(output);
 };
