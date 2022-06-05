5a89ea12fd769c358e2884708df841e5590da6b2
Alexey Palanevich <alexey.palanevich@satelliteinnovations.io>
2022-06-05 08:17:35 +0300

    feat: cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,21 @@
+/**
+ * implement function that parses command line arguments (given in format
+ * `--propName value --prop2Name value2`, you don't need to validate it) and prints them
+ * to the console in the format `propName is value, prop2Name is value2`
+ */
 export const parseArgs = () => {
-    // Write your code here 
+  const rawArgs = process.argv.slice(2);
+  const args = [];
+
+  for (let i = 0; i < rawArgs.length; i += 1) {
+    const current = rawArgs[i];
+
+    if (current.startsWith("--")) {
+      args.push([current]);
+    } else {
+      args[args.length - 1].push(current);
+    }
+  }
+
+  console.log(args.map((current) => current.join(" is ")).join(", "));
 };
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,13 @@
+/**
+ * implement function that parses environment variables with prefix
+ * `RSS_` and prints them to the console in the format
+ * `RSS_name1=value1; RSS_name2=value2`
+ */
 export const parseEnv = () => {
-    // Write your code here 
+  console.log(
+    Object.entries(process.env)
+      .filter(([key]) => key.startsWith("RSS_"))
+      .map(([key, value]) => key + "=" + value)
+      .join("; ")
+  );
 };
